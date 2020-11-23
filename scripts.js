a0c8ca373e485926e8d81dcc15115fd38c3b55f6
Duy <jultqd99@gmail.com>
2020-11-23 15:14:47 +0700

    menu complete

--- scripts.js
+++ scripts.js
@@ -8,3 +8,39 @@ window.onscroll = function () {
         menu.classList.remove('sticky');
     }
 }
+
+const primaryMenuSlide = () => {
+    const primaryMenuTrigger = document.querySelector('.primary-menu-trigger i');
+    const primaryMenu = document.querySelector('.primary-menu');
+
+    primaryMenuTrigger.addEventListener('click', () => {
+        if (primaryMenuTrigger.style.transform == "rotate(90deg)") {
+            primaryMenuTrigger.style.transform = '';
+        } else {
+            primaryMenuTrigger.style.transform = "rotate(90deg)";
+        }
+        primaryMenuTrigger.classList.toggle('fa-times');
+        primaryMenu.classList.toggle('primary-menu-show');
+    });
+}
+
+primaryMenuSlide();
+
+
+const subMenuSlide = () => {
+    const subMenuTrigger = document.querySelectorAll('#sub-menu-trigger');
+
+    subMenuTrigger.forEach((trigger) => {
+        trigger.addEventListener('click', () => {
+            trigger.nextElementSibling.classList.toggle('sub-menu-container-show');
+            if (trigger.style.transform == "rotate(90deg)") {
+                trigger.style.transform = '';
+            } else {
+                trigger.style.transform = "rotate(90deg)";
+            }
+        })
+
+    });
+}
+
+subMenuSlide();
